8ec628de0108c59a4009eecb7cb67025a739b04c
Avekeez <s-ssoetomo@lwsd.org>
2015-09-17 06:54:52 -0700

    qwe

--- Assets/building.cs
+++ Assets/building.cs
@@ -5,7 +5,19 @@ public class building : MonoBehaviour {
 
     void Awake () {
         for (int i = 1; i < 201; i++) {
-            transform.GetChild (i).gameObject.SetActive (false);
+            GameObject obj = transform.GetChild (i).gameObject;
+            //obj.SetActive (false);
+            obj.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezeAll;
         }
+    }
+    void OnCollisionEnter (Collision other) {
+        /*
+        if (other.gameObject.tag == "Snake") {
+            Collider[] col = Physics.OverlapSphere (other.transform.position, 1, 1 << 10);
+            for (int i = 0; i < col.Length; i++) {
+                col[i].gameObject.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.None;
+            }
+        }
+        */
     }
 }
